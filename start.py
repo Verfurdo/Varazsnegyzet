d0adeaa113754ecc086bd6ef76158e3f7b1bb965
API65I <amolnart@gmail.com>
2025-03-12 07:42:17 +0100

    start.py

--- start.py
+++ start.py
@@ -1,9 +1,11 @@
-import os
-import sys
+# start.py: A program belépési pontja, útvonalakat tölt be és elindítja a programot.
+
+import os # Operációs rendszerrel kapcsolatos műveletekhez (fájlok elérési útjának kezelése)
+import sys # Python futtatókörnyezet elérését biztosító modul importálása
 
 # Dinamikus útvonal beállítása
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir)
+current_dir = os.path.dirname(os.path.abspath(__file__)) # A jelenlegi szkript könyvtárának meghatározása
+sys.path.append(current_dir) # A könyvtár a Python modulkeresési útvonalához
 
 # Főprogram indítása
 import main
